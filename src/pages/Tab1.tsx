42687dab96947980c9e7fc9d3ed79bfc6d9427e2
Luisas14 <117782308+Luisas14@users.noreply.github.com>
2024-11-28 17:37:40 -0500

    PEQUEÑOS CAMBIOS
    Agregamos mapa
    cambiamos titulos
    añadimos un search

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,18 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage >
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1 HOLA SOY UN TITULO  </IonTitle>
+
+
         </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        
-        <ExploreContainer name="Tab 1 page me cagss" />
+      </IonHeader >
+      <IonContent fullscreen >
+        <>
+          <IonSearchbar placeholder="Ingresa el nombre de una estación o una dirección"></IonSearchbar>
+        </>
+        <ExploreContainer name="BOGOTÁ D.C. " />
       </IonContent>
     </IonPage>
   );
