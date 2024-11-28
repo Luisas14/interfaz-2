15d313e12a5c2c0c8e3f9b6a12bc6740aab62adf
Luisas14 <117782308+Luisas14@users.noreply.github.com>
2024-11-28 16:45:47 -0500

    ....
    ....

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,16 +7,12 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Tab 1 HOLA SOY UN TITULO  </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        
+        <ExploreContainer name="Tab 1 page me cagss" />
       </IonContent>
     </IonPage>
   );
